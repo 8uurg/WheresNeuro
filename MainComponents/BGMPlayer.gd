5549d3626968df63eb79a4e2ba35c7e0c4830ba7
Arthur <arthur.guijt12@gmail.com>
2023-12-31 13:36:49 +0100

    Tweak volume sliders

--- MainComponents/BGMPlayer.gd
+++ MainComponents/BGMPlayer.gd
@@ -7,10 +7,14 @@ var which_player = 0
 
 func play_bgm(stream):
 	if which_player == 0:
+		# If already playing - don't restart
+		if pl2.stream == stream: return
 		which_player = 1
 		pl1.stream = stream
 		ap.play("FadeTo1")
 	else:
+		# If already playing - don't restart
+		if pl1.stream == stream: return
 		which_player = 0
 		pl2.stream = stream
 		ap.play("FadeTo2")
