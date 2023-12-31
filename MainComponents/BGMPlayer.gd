538d369dfb607a65a30684f04b761613418b41ab
Arthur <arthur.guijt12@gmail.com>
2023-12-31 13:43:49 +0100

    Add quick start for initial BGM

--- MainComponents/BGMPlayer.gd
+++ MainComponents/BGMPlayer.gd
@@ -4,6 +4,7 @@ var which_player = 0
 @onready var pl1 = $Player1
 @onready var pl2 = $Player2
 @onready var ap = $AnimationPlayer
+var initial = true
 
 func play_bgm(stream):
 	if which_player == 0:
@@ -11,7 +12,10 @@ func play_bgm(stream):
 		if pl2.stream == stream: return
 		which_player = 1
 		pl1.stream = stream
-		ap.play("FadeTo1")
+		if initial:
+			ap.play("QuickFade1")
+		else:
+			ap.play("FadeTo1")
 	else:
 		# If already playing - don't restart
 		if pl1.stream == stream: return
